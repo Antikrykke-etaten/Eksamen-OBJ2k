62573b3751384500f24c4cd4ea62f89d0e96f62a
Nichiatu <fristurt@hotmail.com>
2015-12-15 13:11:12 +0100

    storage tool version check and cleanup

--- src/droodle/Droodle.java
+++ src/droodle/Droodle.java
@@ -10,20 +10,16 @@ import droodle.file.Sketch;
 public class Droodle {
 
 	public static void main(String[] args) {
-		List<Sketch> sketchnames;
-		String names = "";
-		try {
-			sketchnames = new FileTreeParser().getSketchNames();
-			for (Sketch sketch : sketchnames) {
-				names += sketch.getSketchName() + " ";
-			}
-			System.out.println(names);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		new SplashScreen();
+		
 		Storage storage = new Storage("sketches-6");
+		String storageVersion = storage.getVersion();
+		
+		if ("Storage tool version 1.2" == storageVersion) {
+			new SplashScreen();
+		} else {
+			throw new SecurityException("Use a newer version of Storagetool");
+		}
+		
 	}
 
 }
