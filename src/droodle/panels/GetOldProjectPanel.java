ff76ae90478e6328fbbaf263b16bb5c78b45b60e
eiriks0 <cam-bridge@hotmail.com>
2015-12-18 12:47:09 +0100

    List
    List

--- src/droodle/panels/GetOldProjectPanel.java
+++ src/droodle/panels/GetOldProjectPanel.java
@@ -108,6 +108,11 @@ public class GetOldProjectPanel extends JPanel {
 		        String a = list.getSelectedValue().toString();
 		        DroodlePanel.sf.sketchName = a;
 		        
+		        if(a != null && !a.isEmpty()){
+		        	 System.out.println("Velg sketch");
+		        	return;	
+		        }
+		        
 		        for (String f:Droodle.storage.getFilenames()) {
 					if(f.equals(DroodlePanel.sf.sketchName)){
 						try {
